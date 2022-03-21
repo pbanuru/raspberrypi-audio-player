b57840e42b003f7c6527a877756ce5f526d44352
user <55062649+pbanuru@users.noreply.github.com>
2022-03-21 11:13:32 -0700

    Experimented to understand how to play audio files with pygame. main.py now plays a random song from the specified folder.

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+from pygame import mixer
+from time import sleep
+import pygame
+import os
+import random
 
 
 if __name__ == '__main__':
     print("Begin")
+    pygame.init()
+    mixer.init()
+
+    # load a random song from audio/Genshin Impact [Music Box]/
+    random_song = random.choice(os.listdir('audio/Genshin Impact [Music Box]/'))
+
+    mixer.music.load('audio/Genshin Impact [Music Box]/' + random_song)
+    mixer.music.play()
+    print("Now playing: " + random_song)
+    
+
+    while mixer.music.get_busy():
+        sleep(1)
+
+    print("End")
+    mixer.quit()
